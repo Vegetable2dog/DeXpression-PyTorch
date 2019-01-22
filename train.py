e5a1161535625623e788040282929001e08d5d6e
Satrajit Chatterjee <satrajitdchatterjee@gmail.com>
2019-01-22 16:20:45 +0530

    Updated train.py
    Minor changes

--- train.py
+++ train.py
@@ -27,6 +27,7 @@ classes = ('Anger', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise',)
 block1 = Block1()
 block2 = Block2()
 block3 = Block3()
+net = nn.Sequential(block1, block2, block3)
 criterion = nn.NLLLoss()  # we want to use NLLLoss over here
 optimizer = optim.Adam(block3.parameters(), lr=0.1, betas=(0.9, 0.999), eps=0.1)
 for epoch in range(EPOCHS):
@@ -35,9 +36,10 @@ for epoch in range(EPOCHS):
     for i, data in enumerate(train_data_loader, 0):
         inputs, labels = data
         optimizer.zero_grad()
-        outputs = block1(inputs)
-        outputs = block2(outputs, outputs)
-        outputs = block3(outputs, outputs)
+        # outputs = block1(inputs)
+        # outputs = block2(outputs, outputs)
+        # outputs = block3(outputs, outputs)
+        outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -45,6 +47,12 @@ for epoch in range(EPOCHS):
         print('[%d, %5d]' % (epoch + 1, i + 1))
     print('EPOCH: %d, LOSS: %.5f' % (epoch + 1, running_loss1/2373.0))  # 18981/8
     running_loss1 = 0.0
+    # saving after each epoch
+    torch.save({'BLOCK_1_state_dict': block1.state_dict(),
+                'BLOCK_2_state_dict': block2.state_dict(),
+                'BLOCK_3_state_dict': block3.state_dict()
+                }, 'last_model_state.pth')
+
 print("Finished Training")
 # saving the final model
 torch.save({'BLOCK_1_state_dict': block1.state_dict(),
